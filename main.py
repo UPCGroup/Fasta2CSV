575afad847b2abe8752119c900d6780d472673db
1i9h7_b1u3 <1012796366@qq.com>
2023-07-22 10:02:38 +0800

    源代码提交

---
+++ main.py
@@ -0,0 +1,26 @@
+from Bio import SeqIO
+import pandas as pd
+import os
+
+def convert(seq):
+    if type(seq) != str:
+        raise TypeError("传入的文件名称必须为字符串")
+    meta = []
+    sequence = []
+    label = []
+    i = 0
+    for seq_record in SeqIO.parse(seq, "fasta"):
+        meta.append(str(seq_record.id))
+        sequence.append(str(seq_record.seq))
+        i = 1 + i
+        label.append(int(i))
+        # print(sequence)
+        df = pd.DataFrame(data={'Meta': meta, 'SequenceID': sequence, 'Label': label})  # 转换后的文件的表头
+        print(df)
+
+        # 数据存入csv
+        df.to_csv(seq[:-6] + ".csv", sep=',', index=False)  # 转换后的文件
+
+# 传入 Fasta 文件位置，会自动保存 csv 文件
+convert("./testdata/testing_proteins_withannotation_STY.fasta")
+convert("./testdata/training_proteins_nonredundant_STY.fasta")
